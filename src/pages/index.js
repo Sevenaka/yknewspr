36510f2f6017bb49dece30949f0e2a3e5a0a6fe8
Sevenaka <Sevenakaoxxy@gmail.com>
2019-06-24 10:22:08 +0600

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Logo from '../images/e771731.png'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class Home extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <header>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="logo-block">
+                  <a href="/">
+                    <img src={Logo} alt="Реклана на YK-news.kz" />
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-8">
+                <nav>
+                  <ul>
+                    <li><a href=""></a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </header>
+        
+      </Layout>
+    )
+  }
+}
 
-export default IndexPage
+export default Home
